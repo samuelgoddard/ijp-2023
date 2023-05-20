e6fc0803d5f5c8894e7c3ed916a5d922e39be0c0
Sam Goddard <hello@samgoddard.co.uk>
2023-05-20 15:02:19 +0100

    authors added

--- pages/_app.js
+++ pages/_app.js
@@ -7,52 +7,74 @@ import SEO from '@/helpers/seo.config';
 import Footer from '@/components/footer';
 import Header from '@/components/header';
 import { IntroContext } from '@/context/intro'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TextScramble } from '@a7sc11u/scramble';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter()
+  const [introText1, setIntroText1] = useState(false)
+  const [introText2, setIntroText2] = useState(false)
+  const [introText3, setIntroText3] = useState(false)
+  const [introText4, setIntroText4] = useState(false)
+  const [introText5, setIntroText5] = useState(false)
+  const [introText6, setIntroText6] = useState(false)
+  const [introText7, setIntroText7] = useState(false)
+  const [introText8, setIntroText8] = useState(false)
+  const [introText9, setIntroText9] = useState(false)
   const [introContext, setIntroContext] = useState(false);
   
   const introEnd = {
     visible: {
       opacity: '100%',
-      transition: {
-        delay: 2.75,
-        duration: 0,
-        ease: [0.83, 0, 0.17, 1]
-      }
     },
     hidden: { 
       opacity: 0,
-      transition: {
-        duration: 0,
-        delay: 2.75,
-        ease: [0.83, 0, 0.17, 1]
-      }
+      transition: { delay: 5, duration: 0, ease: [0.83, 0, 0.17, 1] }
     }
-  }
-
-  const container = {
-    hidden: { opacity: 0, transition: { duration: 0 } },
-    visible: {
-      opacity: 1,
-      transition: {
-        duration: 0,
-        staggerChildren: 0.15
-      }
-    }
-  }
-  
-  const containerItem = {
-    hidden: { opacity: 0, transition: { duration: 0 } },
-    visible: { opacity: 1, transition: { duration: 0 } }
   }
 
   const introFullEnd = {
     visible: { visibility: 'block' },
-    hidden: { visibility: 'hidden' }
+    hidden: { visibility: 'hidden',  }
   }
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setIntroText1(true)
+    }, 0);
+
+    setTimeout(() => {
+      setIntroText2(true)
+    }, 250);
+
+    setTimeout(() => {
+      setIntroText3(true)
+    }, 500);
+
+    setTimeout(() => {
+      setIntroText4(true)
+    }, 750);
+
+    setTimeout(() => {
+      setIntroText5(true)
+    }, 1000);
+    
+    setTimeout(() => {
+      setIntroText6(true)
+    }, 1250);
+    
+    setTimeout(() => {
+      setIntroText7(true)
+    }, 1500);
+
+    setTimeout(() => {
+      setIntroText8(true)
+    }, 1750);
+
+    setTimeout(() => {
+      setIntroText9(true)
+    }, 2000);
+  },[]);
 
   return (
     <>
@@ -62,38 +84,39 @@ export default function App({ Component, pageProps }) {
         <div className={`${AntiqueLegacy.variable} font-sans flex flex-col min-h-screen`}>
           <LazyMotion features={domAnimation}>
           { !introContext && router.asPath == '/' && (
-              <div className="fixed inset-0 z-[100] p-3 px-4 h-screen bg-white text-sm md:text-sm lg:text-sm leading-[1.15] md:leading-[1.15] lg:leading-[1.15] cursor-wait">
+              <div className="fixed inset-0 z-[100] p-3 px-4 h-screen bg-white text-base md:text-base lg:text-base leading-[1.2] md:leading-[1.2] lg:leading-[1.2] cursor-wait">
                 <m.div 
                   initial="visible"
                   animate="hidden"
                   variants={introFullEnd}
-                  transition={{ delay: 2750, duration: 0, ease: [0.83, 0, 0.17, 1] }}
-                  cl1assName="w-full h-full p-[10px] cursor-wait absolute inset-0"
+                  transition={{ delay: 5000, duration: 0, ease: [0.83, 0, 0.17, 1] }}
+                  className="w-full h-full p-[10px] cursor-wait absolute inset-0"
                 >
                   <m.div 
                     initial="visible"
                     animate="hidden"
                     variants={introEnd}
                   >
-                    <m.div variants={container} initial="hidden" animate="visible" className="w-full mt-auto overflow-hidden relative">
-                      <m.span variants={containerItem} className="block">
-                      <TextScramble
-                        as="div"
-                        play={true}
-                        speed={0.75}
-                        scramble={1}
-                        step={1}
-                        stepInterval={1}
-                        seed={3}
-                        seedInterval={1}
-                        text={'INIT X.ROUTE—AAC33ND'}
-                      />
-                      </m.span>
-                      <m.span variants={containerItem} className="block">
+                    <div className="w-full mt-auto overflow-hidden relative">
+                      {introText1 && (
                         <TextScramble
                           as="div"
                           play={true}
-                          speed={0.75}
+                          speed={1.25}
+                          scramble={1}
+                          step={1}
+                          stepInterval={1}
+                          seed={3}
+                          seedInterval={1}
+                          text={'INIT X.ROUTE—AAC33ND'}
+                        />
+                      )}
+
+                      {introText2 && (
+                        <TextScramble
+                          as="div"
+                          play={true}
+                          speed={1.25}
                           scramble={1}
                           step={1}
                           stepInterval={1}
@@ -101,12 +124,12 @@ export default function App({ Component, pageProps }) {
                           seedInterval={1}
                           text={'SEARCHING FOR PAGE ...'}
                         />
-                      </m.span>
-                      <m.span variants={containerItem} className="block">
+                      )}
+                      {introText3 && (
                         <TextScramble
                           as="div"
                           play={true}
-                          speed={0.75}
+                          speed={1.25}
                           scramble={1}
                           step={1}
                           stepInterval={1}
@@ -114,12 +137,12 @@ export default function App({ Component, pageProps }) {
                           seedInterval={1}
                           text={'SEARCHING FOR PAGE ...'}
                         />
-                      </m.span>
-                      <m.span variants={containerItem} className="block">
+                      )}
+                      {introText4 && (
                         <TextScramble
                           as="div"
                           play={true}
-                          speed={0.75}
+                          speed={1.25}
                           scramble={1}
                           step={1}
                           stepInterval={1}
@@ -127,25 +150,25 @@ export default function App({ Component, pageProps }) {
                           seedInterval={1}
                           text={'SEARCHING FOR PAGE ...'}
                         />
-                      </m.span>
-                      <m.span variants={containerItem} className="block">
+                      )}
+                      {introText5 && (
                         <TextScramble
                           as="div"
                           play={true}
-                          speed={0.75}
+                          speed={1.25}
                           scramble={1}
                           step={1}
                           stepInterval={1}
                           seed={3}
                           seedInterval={1}
-                          text={'SEARCHING FOR PAGE ...'}
+                          text={'STILL SEARCHING ...'}
                         />
-                      </m.span>
-                      <m.span variants={containerItem} className="block">
+                      )}
+                      {introText6 && (
                         <TextScramble
                           as="div"
                           play={true}
-                          speed={0.75}
+                          speed={1.25}
                           scramble={1}
                           step={1}
                           stepInterval={1}
@@ -153,12 +176,12 @@ export default function App({ Component, pageProps }) {
                           seedInterval={1}
                           text={'FOUND ...'}
                         />
-                      </m.span>
-                      <m.span variants={containerItem} className="block">
+                      )}
+                      {introText7 && (
                         <TextScramble
                           as="div"
                           play={true}
-                          speed={0.75}
+                          speed={1.25}
                           scramble={1}
                           step={1}
                           stepInterval={1}
@@ -166,12 +189,12 @@ export default function App({ Component, pageProps }) {
                           seedInterval={1}
                           text={'RETURN ... MSG-STATE-INIT'}
                         />
-                      </m.span>
-                      <m.span variants={containerItem} className="block">
+                      )}
+                      {introText8 && (
                         <TextScramble
                           as="div"
                           play={true}
-                          speed={0.75}
+                          speed={1.25}
                           scramble={1}
                           step={1}
                           stepInterval={1}
@@ -179,21 +202,24 @@ export default function App({ Component, pageProps }) {
                           seedInterval={1}
                           text={'IJP-23 RETURN ...'}
                         />
-                      </m.span>
-                      <m.span variants={containerItem} className="block">
-                        <TextScramble
-                          as="div"
-                          play={true}
-                          speed={0.75}
-                          scramble={1}
-                          step={1}
-                          stepInterval={1}
-                          seed={3}
-                          seedInterval={1}
-                          text={'IJPOWELL.CO.UK'}
-                        />
-                      </m.span>
-                    </m.div>
+                      )}
+                      {introText9 && (
+                        <>
+                          <TextScramble
+                            as="span"
+                            play={true}
+                            speed={1.25}
+                            scramble={1}
+                            step={1}
+                            stepInterval={1}
+                            seed={3}
+                            seedInterval={1}
+                            text={'IJPOWELL.CO.UK'}
+                          />
+                          <span className="inline cursor">...</span>
+                        </>
+                      )}
+                    </div>
                   </m.div>
                 </m.div>
               </div>
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   useEffect(() => {
     setTimeout(() => {
       setIntroContext(true)
-    }, 2750);
+    }, 3000);
   },[]);
 
   return (
