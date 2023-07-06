c1d27d1fa8c4d549ae7f0d1e0b5941fab3fa81de
samuelgoddard <hello@samgoddard.co.uk>
2023-07-06 11:23:19 +0100

    testing

--- helpers/seo.config.js
+++ helpers/seo.config.js
@@ -1,6 +1,6 @@
 export default {
   defaultTitle: 'Interaction, Brand, Art Direction - Isaac Powell',
-  description: 'A simple Next.js starter kit with Tailwind and Framer Motion.',
+  description: 'Based in England but working worldwide, I have spent the last six years collaborating with brands, agencies and some extremely talented individuals to create a variety of interactive websites and visual identities.',
   titleTemplate: '%s | Isaac Powell',
   openGraph: {
     type: 'website',
