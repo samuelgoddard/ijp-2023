3943b7786bce126e6c6969e36446b45167216433
samuelgoddard <hello@samgoddard.co.uk>
2023-07-06 17:00:16 +0100

    go live

--- helpers/seo.config.js
+++ helpers/seo.config.js
@@ -1,15 +1,15 @@
 export default {
-  defaultTitle: 'Interaction, Brand, Art Direction - Isaac Powell',
+  defaultTitle: 'Interaction, Brand, Art Direction | Isaac Powell',
   description: 'Based in England but working worldwide, I have spent the last six years collaborating with brands, agencies and some extremely talented individuals to create a variety of interactive websites and visual identities.',
   titleTemplate: '%s | Isaac Powell',
   openGraph: {
     type: 'website',
     locale: 'en_GB',
     url: 'https://ijpowell.co.uk/',
-    site_name: 'Interaction, Brand, Art Direction - Isaac Powell',
+    site_name: 'Interaction, Brand, Art Direction | Isaac Powell',
     images: [
       {
-        url: 'https://ijp-2023.vercel.app/images/social.jpg',
+        url: 'https://ijpowell.co.uk/images/social.jpg',
         width: 1200,
         height: 630,
         alt: `Isaac Powell Logo`,
